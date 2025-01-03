301befcb1888ab86815e704920068dde9916837b
jerrychoux <jerrychoux@163.com>
2025-01-03 17:56:58 +0800

    update versionGenerator

--- scripts/versionGenerator.ts
+++ scripts/versionGenerator.ts
@@ -1,15 +1,32 @@
+import * as fs from "fs";
+import * as crypto from "crypto";
 import * as semver from "semver";
-import yargs, { Options } from "yargs";
+import yargs, { Arguments, Options } from "yargs";
 import { tryit } from "radash";
 
 import { isValidPath } from "./utils/path";
+import path from "path";
 // args
-const argsOptions: Record<string, Options> = {
+interface Args {
+  version: string;
+  url: string;
+  src: string;
+  dest: string;
+}
+
+const defaultArgs: Partial<Args> = {
+  version: "1.0.0",
+  url: "http://localhost",
+  src: "./jsb/",
+  dest: "./remote-assets/",
+};
+
+const options: Record<keyof Args, Options> = {
   version: {
     alias: "v",
     type: "string",
+    default: defaultArgs.version,
     description: "The version of published assets",
-    demandOption: true,
     coerce: (arg: string) => {
       if (!semver.valid(arg)) {
         throw new Error(
@@ -22,19 +39,19 @@ const argsOptions: Record<string, Options> = {
   url: {
     alias: "u",
     type: "string",
+    default: defaultArgs.url,
     description: "The url of remote server",
-    demandOption: false,
     coerce: (arg: string) => {
       const [error] = tryit(() => new URL(arg))();
       if (error) throw new Error("Invalid URL provided");
       return arg;
     },
   },
-  source: {
+  src: {
     alias: "s",
     type: "string",
+    default: defaultArgs.src,
     description: "The source path of assets",
-    demandOption: true,
     coerce: (arg: string) => {
       if (!isValidPath(arg)) {
         throw new Error("Invalid source path provided");
@@ -42,11 +59,11 @@ const argsOptions: Record<string, Options> = {
       return arg;
     },
   },
-  destination: {
+  dest: {
     alias: "d",
     type: "string",
+    default: defaultArgs.dest,
     description: "The destination path of assets",
-    demandOption: true,
     coerce: (arg: string) => {
       if (!isValidPath(arg)) {
         throw new Error("Invalid destination path provided");
@@ -56,8 +73,6 @@ const argsOptions: Record<string, Options> = {
   },
 };
 
-const argv = yargs.version(false).options(argsOptions).argv;
-
 interface Manifest {
   packageUrl: string;
   remoteManifestUrl: string;
@@ -66,3 +81,85 @@ interface Manifest {
   assets: {};
   searchPaths: string[];
 }
+
+const argv = yargs.version(false).options(options).argv as Arguments<Args>;
+
+const manifest: Manifest = {
+  packageUrl: argv.url,
+  remoteManifestUrl: `${argv.url}/project.manifest`,
+  remoteVersionUrl: `${argv.url}/version.manifest`,
+  version: argv.version,
+  assets: {},
+  searchPaths: [],
+};
+
+interface Asset {
+  size: number;
+  md5: string;
+  compressed?: boolean;
+}
+
+const readAssets = (dir: string, obj: { [key: string]: Asset }) => {
+  try {
+    const state = fs.statSync(dir);
+    if (!state.isDirectory()) {
+      return;
+    }
+
+    const subdirs = fs.readdirSync(dir);
+    subdirs.forEach((subdir) => {
+      // ignore hidden files
+      if (subdir.startsWith(".")) {
+        return;
+      }
+
+      const subPath = path.join(dir, subdir);
+      const state = fs.statSync(subPath);
+      if (state.isDirectory()) {
+        readAssets(subPath, obj);
+      } else if (state.isFile()) {
+        const size = state.size;
+        const md5 = crypto
+          .createHash("md5")
+          .update(fs.readFileSync(subPath))
+          .digest("hex");
+        const compressed = path.extname(subdir) === ".zip";
+        const relativePath = encodeURI(
+          path.relative(argv.src, subPath).replace(/\\/g, "/")
+        );
+
+        obj[relativePath] = {
+          size,
+          md5,
+        };
+        if (compressed) {
+          obj[relativePath].compressed = true;
+        }
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+readAssets(path.join(argv.src, "src"), manifest.assets);
+readAssets(path.join(argv.src, "assets"), manifest.assets);
+readAssets(path.join(argv.src, "jsb-adapter"), manifest.assets);
+
+const destPath = path.join(argv.dest, "project.manifest");
+const versionPath = path.join(argv.dest, "version.manifest");
+
+fs.mkdirSync(argv.dest, { recursive: true });
+fs.writeFileSync(destPath, JSON.stringify(manifest, null, 2));
+
+type ModifiedManifest = Omit<Manifest, "assets" | "searchPaths"> & {
+  assets?: Manifest["assets"];
+  searchPaths?: Manifest["searchPaths"];
+};
+
+delete (manifest as ModifiedManifest).assets;
+delete (manifest as ModifiedManifest).searchPaths;
+
+fs.writeFileSync(versionPath, JSON.stringify(manifest, null, 2));
+
+console.log(`Manifest files generated at ${argv.dest}`);
