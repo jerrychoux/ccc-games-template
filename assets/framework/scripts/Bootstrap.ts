286e9839361ba280a0dcc557d842dbd550c3a1d0
jerrychoux <jerrychoux@silveruc.com>
2025-08-01 11:59:52 +0800

    add some assets

---
+++ assets/framework/scripts/Bootstrap.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("Bootstrap")
+export class Bootstrap extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
